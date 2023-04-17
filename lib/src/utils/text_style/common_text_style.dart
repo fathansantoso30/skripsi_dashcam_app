b08b7e2c8f5b90258f1ac8b2b4462810e115f1ea
Fathan Santoso <fathansantoso30@gmail.com>
2023-04-18 06:07:34 +0700

    added style from figma design

---
+++ lib/src/utils/text_style/common_text_style.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+TextStyle get titleH1 => const TextStyle(
+      fontSize: 48,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 56 / 48,
+      letterSpacing: 0,
+    );
+
+TextStyle get titleH2 => const TextStyle(
+      fontSize: 40,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 48 / 40,
+      letterSpacing: 0,
+    );
+
+TextStyle get titleH3 => const TextStyle(
+      fontSize: 32,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 40 / 32,
+      letterSpacing: 0,
+    );
+
+TextStyle get titleH4 => const TextStyle(
+      fontSize: 24,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 32 / 24,
+      letterSpacing: 0,
+    );
+
+TextStyle get titleH5 => const TextStyle(
+      fontSize: 20,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 28 / 20,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodyLregular => const TextStyle(
+      fontSize: 18,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 26 / 18,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodyLmedium => const TextStyle(
+      fontSize: 18,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 26 / 18,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodyMregular => const TextStyle(
+      fontSize: 16,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodyMmedium => const TextStyle(
+      fontSize: 16,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodySregular => const TextStyle(
+      fontSize: 14,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 22 / 14,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodySmedium => const TextStyle(
+      fontSize: 14,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 22 / 14,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodyXSregular => const TextStyle(
+      fontSize: 12,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      height: 20 / 12,
+      letterSpacing: 0,
+    );
+
+TextStyle get bodyXSmedium => const TextStyle(
+      fontSize: 12,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 20 / 12,
+      letterSpacing: 0,
+    );
+
+TextStyle get buttonL => const TextStyle(
+      fontSize: 18,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 26 / 18,
+      letterSpacing: 0,
+    );
+
+TextStyle get buttonM => const TextStyle(
+      fontSize: 16,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 24 / 16,
+      letterSpacing: 0,
+    );
+
+TextStyle get buttonS => const TextStyle(
+      fontSize: 14,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      height: 22 / 14,
+      letterSpacing: 0,
+    );
+
+TextStyle get interfacesectionheading => const TextStyle(
+      fontSize: 11,
+      decoration: TextDecoration.none,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w700,
+      height: 19 / 11,
+      letterSpacing: 15,
+    );
