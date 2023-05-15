18d252f7074d83a4b54ea3da59b760a64eebb8f4
Fathan Santoso <fathansantoso30@gmail.com>
2023-05-16 01:47:34 +0700

    refactor import

--- lib/features/live_stream/domain/repositories/live_stream_repository.dart
+++ lib/features/live_stream/domain/repositories/live_stream_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import '../entities/live_stream_entity.dart';
 
 import '../../../../core/error/failures.dart';
+import '../entities/live_stream_entity.dart';
 
 abstract class LiveStreamRepository {
   Future<Either<Failure, LiveStreamEntity>> getLiveStreamData();
