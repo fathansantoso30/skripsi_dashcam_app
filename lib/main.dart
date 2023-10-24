ab71f6337e361b49869727a12d9246ba1377742b
Fathan Santoso <fathansantoso30@gmail.com>
2023-10-24 11:45:24 +0700

    changes to main

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,8 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  static const String url = "ws://192.168.4.1:8888";
-  WebSocketChannel? _channel;
-  bool _isConnected = false;
-
-  void connect() {
-    _channel = IOWebSocketChannel.connect(Uri.parse(url));
-    setState(() {
-      _isConnected = true;
-    });
-  }
-
-  void disconnect() {
-    _channel!.sink.close();
-    setState(() {
-      _isConnected = false;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
