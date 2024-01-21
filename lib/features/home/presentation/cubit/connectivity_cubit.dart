0b053fab9869d541a87ed21d9cf237eb3756ebed
Fathan Hudyaussie Santoso <fathansantoso30@gmail.com>
2024-01-21 15:24:22 +0700

    added location check on start

--- lib/features/home/presentation/cubit/connectivity_cubit.dart
+++ lib/features/home/presentation/cubit/connectivity_cubit.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 
 part 'connectivity_state.dart';
@@ -12,6 +13,10 @@ class ConnectivityCubit extends Cubit<ConnectivityState> {
   bool isConnected = false;
 
   final String targetSSID = 'ESP32-CAM';
+  Location location = Location();
+
+  bool? serviceEnabled;
+  PermissionStatus? permissionGranted;
 
   Future<void> checkWifiStatus() async {
     // emit(ConnectLoadingState());
@@ -30,6 +35,24 @@ class ConnectivityCubit extends Cubit<ConnectivityState> {
       }
     } catch (e) {
       log("Error checking Wi-Fi status: $e");
+    }
+  }
+
+  Future<void> checkLocationService() async {
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled!) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled!) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
     }
   }
 }
